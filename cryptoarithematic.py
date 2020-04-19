aacc362f0fc00cc370a82af01f716af4f849b5e1
Preeti Bhandari <preeti1205@users.noreply.github.com>
2020-04-18 20:49:00 -0400

    refactor

--- cryptoarithematic.py
+++ cryptoarithematic.py
@@ -1,14 +1,9 @@
-# -------------
-# User Instructions
-#
-# Complete the fill_in(formula) function by adding your code to
-# the two places marked with ?????. 
-
 import string, re, itertools
 
 def solve(formula):
     """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
     Input formula is a string; output is a digit-filled-in string or None."""
+    compile_word('YOU')
     for f in fill_in(formula):
         if valid(f):
             return f
@@ -29,3 +24,41 @@ def valid(f):
         return not re.search(r'\b0[0-9]', f) and eval(f) is True
     except ArithmeticError:
         return False
+
+def compile_word(word):
+    """Compile a word of uppercase letters as numeric digits.
+    E.g., compile_word('YOU') => '(1*U+10*O+100*Y)'
+    Non-uppercase words unchanged: compile_word('+') => '+'"""
+    # Your code here.
+    #better solution
+    if not word.isupper():
+        return word
+    else:
+        compiled_word = [ ('%s*%s' %(10**i, ch))
+                            for i, ch in enumerate(word[::-1]) ]
+    new_word =  '(' + '+'.join(compiled_word) + ')'
+    print new_word
+    return new_word
+
+
+    #my solution
+    # compiled_word = []
+    # power = 0;
+    # if not word.isupper():
+    #     return word
+
+    # for i,ch in enumerate(word[::-1]): #from the back
+    #     if not ch.isalpha:
+    #         compiled_word.append(ch)
+    #     else:
+    #         form = str(10**i) + '*' + ch
+    #         compiled_word.append(form)
+    
+    # compiled_word = '(' + '+'.join(compiled_word) + ')'
+    # print compiled_word
+
+    # return compiled_word
+
+
+if __name__ == "__main__":
+    solve('A + B == C**2')
